db2c1d2f7ffec8af246d7a5a5e18297a6ca1fa5a
skysplatwoomy <94260735+skysplatwoomy@users.noreply.github.com>
2022-09-16 22:03:15 +0100

    debloating

--- source code.py
+++ source code.py
@@ -1,26 +1,14 @@
-#whole stand-alone calculator
-import sys
-def calc():
-  def loop():
-    retry = input("Do you want to exit? Type 'Y' or 'N'. ").upper()
-    if retry == "N":
-      print("Retrying...")
-      calc()
-    elif retry == "Y":
-      sys.exit(0)
-    else:
-      print("Invalid option.")
-      loop()
-  def factorial(n):
-    answer = 1
-    for x in range(1, n+1):
-      answer = x*answer
+def factorial(n):
+  answer = 1
+  for x in range(1, n+1):
+    answer = x*answer
     return answer
-  try:
-    n = int(input("factorial calculator: "))
-  except:
-    print("Error found")
-    loop()
-  print(f"{n} factorial = {factorial(n)}")
-  loop()
-calc()
+try:
+  n = int(input("factorial calculator: "))
+except ValueError:
+  print("word!, me too")
+  from sys import exit
+  input("press enter to exit ")
+  exit(0)
+print(f"{n} factorial = {factorial(n)}")
+input("press enter to exit")
