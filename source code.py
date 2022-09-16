4b20787e7bfb2dd46b04843ab7c377eeda2982a7
skysplatwoomy <94260735+skysplatwoomy@users.noreply.github.com>
2022-09-16 22:06:19 +0100

    debugging

--- source code.py
+++ source code.py
@@ -1,14 +1,14 @@
 def factorial(n):
-  answer = 1
-  for x in range(1, n+1):
-    answer = x*answer
+    answer = 1
+    for x in range(1, n+1):
+        answer = x*answer
     return answer
 try:
-  n = int(input("factorial calculator: "))
+    n = int(input("factorial calculator: "))
 except ValueError:
-  print("word!, me too")
-  from sys import exit
-  input("press enter to exit ")
-  exit(0)
+    print("word!, me too")
+    from sys import exit
+    input("press enter to exit ")
+    exit(0)
 print(f"{n} factorial = {factorial(n)}")
-input("press enter to exit")
+input("press enter to exit ")
